097b2aca6b29e8058b1b21cdb2b065456d6af9b6
wsj0715 <dhkdthwls@naver.com>
2023-05-20 02:19:34 +0900

    변경

--- src/pages/MainScreen.jsx
+++ src/pages/MainScreen.jsx
@@ -1,13 +1,59 @@
 import React from "react";
 
 import Header from "../Components/header";
+import '../CSS/Main.css'
 
 function MainScreen(){
     return(
         <div>
             <Header/>
-            <h6>제작 게임</h6>
+
+            <div className="title">
+                <h6>제작 게임</h6>
+            </div>
+
             <hr/>
+
+            <div className="games">
+                <ul>
+                    <li><a href="#"><img src="https://via.placeholder.com/270x270" alt="undefined"/></a></li>
+                    <li><a href="#"><img src="https://via.placeholder.com/270x270" alt="undefined"/></a></li>
+                    <li><a href="#"><img src="https://via.placeholder.com/270x270" alt="undefined"/></a></li>
+                    <li><a href="#"><img src="https://via.placeholder.com/270x270" alt="undefined"/></a></li>
+                    <li><a href="#"><img src="https://via.placeholder.com/270x270" alt="undefined"/></a></li>
+                    <li><a href="#"><img src="https://via.placeholder.com/270x270" alt="undefined"/></a></li>
+                    <li><a href="#"><img src="https://via.placeholder.com/270x270" alt="undefined"/></a></li>
+                    <li><a href="#"><img src="https://via.placeholder.com/270x270" alt="undefined"/></a></li>
+                    <li><a href="#"><img src="https://via.placeholder.com/270x270" alt="undefined"/></a></li>
+                    <li><a href="#"><img src="https://via.placeholder.com/270x270" alt="undefined"/></a></li>
+                    <li><a href="#"><img src="https://via.placeholder.com/270x270" alt="undefined"/></a></li>
+                    <li><a href="#"><img src="https://via.placeholder.com/270x270" alt="undefined"/></a></li>
+                </ul>
+            </div>
+
+            <div className="title2">
+                <h6>사이트 게임</h6>
+            </div>
+
+            <hr/>
+
+            <div className="games">
+                <ul>
+                    <li><a href="#"><img src="https://via.placeholder.com/270x270" alt="undefined"/></a></li>
+                    <li><a href="#"><img src="https://via.placeholder.com/270x270" alt="undefined"/></a></li>
+                    <li><a href="#"><img src="https://via.placeholder.com/270x270" alt="undefined"/></a></li>
+                    <li><a href="#"><img src="https://via.placeholder.com/270x270" alt="undefined"/></a></li>
+                    <li><a href="#"><img src="https://via.placeholder.com/270x270" alt="undefined"/></a></li>
+                    <li><a href="#"><img src="https://via.placeholder.com/270x270" alt="undefined"/></a></li>
+                    <li><a href="#"><img src="https://via.placeholder.com/270x270" alt="undefined"/></a></li>
+                    <li><a href="#"><img src="https://via.placeholder.com/270x270" alt="undefined"/></a></li>
+                    <li><a href="#"><img src="https://via.placeholder.com/270x270" alt="undefined"/></a></li>
+                    <li><a href="#"><img src="https://via.placeholder.com/270x270" alt="undefined"/></a></li>
+                    <li><a href="#"><img src="https://via.placeholder.com/270x270" alt="undefined"/></a></li>
+                    <li><a href="#"><img src="https://via.placeholder.com/270x270" alt="undefined"/></a></li>
+                </ul>
+            </div>
+
         </div>
     )
 }
