1d377fc5436c12ad56a8bb6a686b2d6c0cea4b2b
hseunghwan <hsh9298080@naver.com>
2023-05-25 16:07:23 +0900

    swf 임포트

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,10 +1,11 @@
-import React, {useState} from "react";
-import {AiOutlineStar, AiFillStar, AiOutlineLike, AiFillLike} from 'react-icons/ai';
-
-import '../CSS/Game.css'
+import React, { useState } from "react";
+import { AiOutlineStar, AiFillStar, AiOutlineLike, AiFillLike } from "react-icons/ai";
+import { useParams } from "react-router-dom";
+import "../CSS/Game.css";
 import Header from "../Components/header";
-
-function GameScreen(){
+import RufflePlayer from "../Components/RufflePlayer";
+function GameScreen() {
+    const { id } = useParams();
     const [like, setLike] = useState(false);
     const [star, setStar] = useState(false);
 
@@ -15,7 +16,7 @@ function GameScreen(){
     const toggleStar = () => {
         setStar(!star);
     };
-    return(
+    return (
         <div className="gamescreen">
             <Header />
 
@@ -23,25 +24,27 @@ function GameScreen(){
                 <h6>(게임 이름)</h6>
             </div>
 
-            <hr/>
+            <hr />
 
             <div className="game">
-
+                <RufflePlayer swfUrl={`/resource/swf/${id}.swf`} />
             </div>
 
             <div className="sub">
                 <p>(게임 이름)</p>
 
                 <div className="icon">
-                {star ? <AiFillStar onClick={toggleStar} /> 
-                    : <AiOutlineStar onClick={toggleStar} />}
-                    
-                {like ? <AiFillLike style={{ marginLeft: 10 }} onClick={toggleLike} /> 
-                    : <AiOutlineLike style={{ marginLeft: 10 }} onClick={toggleLike} />}
+                    {star ? <AiFillStar onClick={toggleStar} /> : <AiOutlineStar onClick={toggleStar} />}
+
+                    {like ? (
+                        <AiFillLike style={{ marginLeft: 10 }} onClick={toggleLike} />
+                    ) : (
+                        <AiOutlineLike style={{ marginLeft: 10 }} onClick={toggleLike} />
+                    )}
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default GameScreen;
