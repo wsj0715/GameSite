061ef1408d7295aded3855941700e89232473816
wsj0715 <dhkdthwls@naver.com>
2023-05-25 16:14:37 +0900

    변경

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -7,20 +7,13 @@ import '../CSS/SignIn.css'
 function SignInScreen(){
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
     
     const handleSubmit = (e) => {
         e.preventDefault(); // 폼(submit) 기본 동작 방지
     
         // 입력값 확인 로직
-        if (!username || !password || !confirmPassword) {
+        if (!username || !password ) {
         alert('모든 필드를 입력해주세요.');
-        return;
-        }
-    
-        // 패스워드 일치 확인 로직
-        if (password !== confirmPassword) {
-        alert('비밀번호와 비밀번호 확인이 일치하지 않습니다.');
         return;
         }
     };
