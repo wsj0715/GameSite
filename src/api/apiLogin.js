ee9639b440ae84682175e7d41f2567b6c7b45c83
wsj0715 <dhkdthwls@naver.com>
2023-05-27 02:45:21 +0900

    login

--- src/api/apiLogin.js
+++ src/api/apiLogin.js
@@ -4,6 +4,9 @@ export const Login = async (postReq) => {
     return await apiClient({
         method: "post",
         url: `/auth/login`,
+        headers: {
+            'Content-Type': 'x-www-unlemcoded'
+        },
         data: {
             username: postReq.username,
             password: postReq.password,
