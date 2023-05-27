3d564fdee274fcb8ce6b0526614f536b141a14c7
wsj0715 <dhkdthwls@naver.com>
2023-05-27 15:10:25 +0900

    .

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -2,7 +2,6 @@
 import React, {useState, useEffect} from "react";
 import { useNavigate } from "react-router-dom";
 import { AiFillAliwangwang } from "react-icons/ai";
-import { useDispatch } from "react-redux";
 import axios from "axios";
 
 import { Login } from "../api/apiLogin";
