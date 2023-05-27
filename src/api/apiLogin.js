8906751ac7a30f7793c8cab703373c56a67f9d54
wsj0715 <dhkdthwls@naver.com>
2023-05-27 19:16:40 +0900

    login

--- src/api/apiLogin.js
+++ src/api/apiLogin.js
@@ -4,7 +4,7 @@ import qs from "qs";
 export const Login = async (body) => {
     return await apiClient({
         method: "post",
-        url: `/auth/login`,
+        url: `auth/login`,
         data: qs.stringify(body),
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
