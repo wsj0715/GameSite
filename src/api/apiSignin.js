11f35f0564e8d5f81c6319779ea39ebd219fe065
wsj0715 <dhkdthwls@naver.com>
2023-05-27 18:24:42 +0900

    회원가입

--- src/api/apiSignin.js
+++ src/api/apiSignin.js
@@ -1,8 +1,13 @@
 import { apiClient } from "./apiClient"
+import qs from "qs";
 
-export const Signin = async () => {
+export const Signin = async (body) => {
     return await apiClient({
         method: "post",
-        url: `/auth/signup`
+        url: `/auth/signup`,
+        data: qs.stringify(body),
+        headers: {
+            'Content-Type': 'application/json'
+        },
     })
 }  
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -3,60 +3,33 @@ import React, {useState, useEffect} from "react";
 import { useNavigate } from "react-router-dom";
 import { AiFillAliwangwang } from "react-icons/ai";
 
-import axios from "axios";
-
 import { Signin } from "../api/apiSignin";
 import '../CSS/SignIn.css'
-import { create } from "@mui/material/styles/createTransitions";
 
 function SignInScreen(){
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [email, setEmail] = useState('');
 
-
-    const body = new FormData();
-
-    const postReq = {
+    const body = {
         username: username,
         email: email,
         password: password
-    };
-
-    useEffect(()=>{
-        body.append("username", username);
-        body.append("email", email);
-        body.append("password", password)
-    })
-
-    const API = axios.create({
-        baseURL: 'http://54.180.13.188:8080',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
+    }
 
     const SigninHandle = () => {
-        if(!username || !password || !email) {
-            window.alert("모든 필드를 입력하세요.");
+        if(!username || !password || !email){
+            window.alert("모든 필드를 입력해주세요.");
             return;
         }
-
-        API.post('/auth/signup', {
-            username: postReq.username,
-            email: postReq.email,
-            password: postReq.password
+        Signin(body)
+        .then(response => {
+            console.log(response.data);
+            movePage('/LoginScreen')
         })
-        .then((res)=>{
-            if(res.data.code === 200){
-                window.alert("회원가입 완료!");
-                console.log(res.data)
-                movePage("/LoginScreen");
-            }
-        })
-        .catch((error)=>{
-            console.log(error)
-        })
+        .catch(error => {
+            console.log(error);
+        });
     }
 
     const movePage = useNavigate();
