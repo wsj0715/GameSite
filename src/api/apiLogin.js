d47e38f4e329da77519863b0670430cb5abd3103
hseunghwan <hsh9298080@naver.com>
2023-05-28 20:31:34 +0900

    Merge branch

--- src/api/apiLogin.js
+++ src/api/apiLogin.js
@@ -1,10 +1,6 @@
 import { apiClient } from "./apiClient";
 
-<<<<<<< HEAD
 export const Login = async (data) => {
-=======
-export const login = async (data) => {
->>>>>>> e15031f9a085cc03e7bed15db7485caf9295abc3
     return await apiClient({
         method: "post",
         url: `/auth/login`,
@@ -12,9 +8,6 @@ export const login = async (data) => {
             "Content-Type": "application/x-www-form-urlencoded",
         },
         data: data,
-<<<<<<< HEAD
         withCredentials: true,
-=======
->>>>>>> e15031f9a085cc03e7bed15db7485caf9295abc3
     });
 };
