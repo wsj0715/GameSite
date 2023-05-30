ccfed273ecda3e60b01ff9ef0fdd0036744a2e8c
chosun99 <chosy0406@naver.com>
2023-05-30 21:51:03 +0900

    add game

--- src/pages/MadeGame.jsx
+++ src/pages/MadeGame.jsx
@@ -4,72 +4,87 @@ import { useParams } from "react-router-dom";
 import "../CSS/MadeGame.css";
 import Header from "../Components/header";
 import { Unity, useUnityContext } from "react-unity-webgl";
+
 function MadeGame() {
-    const { id } = useParams();
-    const [like, setLike] = useState(false);
-    const [star, setStar] = useState(false);
+  const { id } = useParams();
+  const [like, setLike] = useState(false);
+  const [star, setStar] = useState(false);
 
-    function getGameName(id) {
-        let gameName;
-        switch (id) {
-            case "1":
-                gameName = "플래피 버드";
-                break;
-            case "2":
-                gameName = "언데드 서바이버";
-                break;
-            case "3":
-                gameName = "테트리스";
-                break;
-            default:
-                gameName = "게임?";
-        }
-        return gameName;
+  function getGameName(id) {
+    let gameName;
+    switch (id) {
+      case "1":
+        gameName = "플래피 버드";
+        break;
+      case "2":
+        gameName = "언데드 서바이버";
+        break;
+      case "3":
+        gameName = "테트리스";
+        break;
+      default:
+        gameName = "게임?";
     }
-    const gameName = getGameName(id);
+    return gameName;
+  }
 
-    const toggleLike = () => {
-        setLike(!like);
-    };
+  const gameName = getGameName(id);
 
-    const toggleStar = () => {
-        setStar(!star);
-    };
+  const toggleLike = () => {
+    setLike(!like);
+  };
 
-    const { unityProvider } = useUnityContext({
-        loaderUrl: `/build${id}/build.loader.js`,
-        dataUrl: `/build${id}/build.data`,
-        frameworkUrl: `/build${id}/build.framework.js`,
-        codeUrl: `/build${id}/build.wasm`,
-    });
+  const toggleStar = () => {
+    setStar(!star);
+  };
 
+  const { unityProvider } = useUnityContext({
+    loaderUrl: `/build${id}/build.loader.js`,
+    dataUrl: `/build${id}/build.data`,
+    frameworkUrl: `/build${id}/build.framework.js`,
+    codeUrl: `/build${id}/build.wasm`,
+  });
+
+  try {
     return (
-        <div className="gamescreen">
-            <Header />
+      <div className="gamescreen">
+        <Header />
 
-            <div className="title">
-                <h6>{gameName}</h6>
-            </div>
-
-            <hr />
-
-            <div className="madegame">{unityProvider && <Unity unityProvider={unityProvider} style={{ width: "750px", height: "500px" }} />}</div>
-
-            <div className="sub">
-                <p>{gameName}</p>
-
-                <div className="icon">
-                    {star ? <AiFillStar onClick={toggleStar} /> : <AiOutlineStar onClick={toggleStar} />}
-
-                    {like ? (
-                        <AiFillLike style={{ marginLeft: 10 }} onClick={toggleLike} />
-                    ) : (
-                        <AiOutlineLike style={{ marginLeft: 10 }} onClick={toggleLike} />
-                    )}
-                </div>
-            </div>
+        <div className="title">
+          <h6>{gameName}</h6>
         </div>
+
+        <hr />
+        {id === "1" && (
+          <div className="madegame1">
+            {unityProvider && <Unity unityProvider={unityProvider} style={{ width: "750px", height: "500px" }} />}
+          </div>
+        )}
+        {id === "2" && (
+          <div className="madegame2">
+            {unityProvider && <Unity unityProvider={unityProvider} style={{ width: "509px", height: "905px" }} />}
+          </div>
+        )}
+        <div className="sub">
+          <p>{gameName}</p>
+
+          <div className="icon">
+            {star ? <AiFillStar onClick={toggleStar} /> : <AiOutlineStar onClick={toggleStar} />}
+
+            {like ? <AiFillLike style={{ marginLeft: 10 }} onClick={toggleLike} /> : <AiOutlineLike style={{ marginLeft: 10 }} onClick={toggleLike} />}
+          </div>
+        </div>
+      </div>
     );
+  } catch (error) {
+    console.error("An error occurred:", error);
+    return (
+      <div className="gamescreen">
+        <Header />
+        <div className="error-message">An error occurred. Please try again later.</div>
+      </div>
+    );
+  }
 }
 
 export default MadeGame;
