45d44a4617172e1c56c912a1e6f3c5bff1c801a8
hseunghwan <hsh9298080@naver.com>
2023-05-31 14:35:01 +0900

    검색

--- src/Components/HomeContentList.jsx
+++ src/Components/HomeContentList.jsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Box, Typography } from "@mui/material";
 import ContentBox from "./ContentBox";
 
 //페이지 안에서 게임 리스트를 보여주는 컴포넌트
 const HomeContentList = ({ contents }) => {
+    const [gameList, setGameList] = useState(contents); // 게임 목록
     //받아온 contents로 img
+    useEffect(() => {
+        setGameList(contents);
+    }, [contents]);
     return (
         <Box sx={{ display: "flex", justifyContent: "space-evenly", flexWrap: "wrap" }}>
-            {contents.map((content) => (
-                <Box key={content.id} sx={{ width: "25%", padding: "5px", maxWidth: "270px" }}>
-                    <ContentBox id={content.id}></ContentBox>
-                    <Typography sx={{ color: "#D9D9D9", whiteSpace: "normal", fontSize: "0.7rem", width: "100%" }}>{content.name}</Typography>
+            {gameList.map((game) => (
+                <Box key={game.id} sx={{ width: "25%", margin: "1rem", maxWidth: "270px" }}>
+                    <ContentBox id={game.id}></ContentBox>
+                    <Typography sx={{ color: "#D9D9D9", whiteSpace: "normal", fontSize: "0.7rem", width: "100%" }}>{game.name}</Typography>
                 </Box>
             ))}
         </Box>
