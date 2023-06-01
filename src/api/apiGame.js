f7507520e223a9d181e35b9b5518e48f45188025
hseunghwan <hsh9298080@naver.com>
2023-06-01 21:05:51 +0900

    test

--- src/api/apiGame.js
+++ src/api/apiGame.js
@@ -1,14 +1,17 @@
 import { apiClient } from "./apiClient";
 
 export const getAllGames = async () => {
-    return await apiClient({
-        method: "get",
-    });
+    // return await apiClient({
+    //     method: "get",
+    //     url: `/`,
+    // });
+    return await apiClient.get();
 };
 
 export const getSearchGames = async (keyword) => {
     return await apiClient({
         method: "get",
+        url: `/`,
         params: {
             name: keyword,
         },
